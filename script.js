3d7aa86267ee716bee675736d7a668e0ad4b13e0
Horkustine <tyler.carneschristie@stu.barren.kyschools.us>
2024-01-12 09:56:39 -0600

    Your mother

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log(12)
+// numbers string boolean null undefined
+
+// + , - , /,  * ,  ** ,%
+console.log(5 % 4)
+console.log(5 % 2)
+
+console.log('I\'M MARY POPPINS, Y\'ALL!') 
+
+console.log('Hawk'+'eye')
+
+console.log('Tyler'.length + "Carnes-Christie".length)
+console.log('Avengers: Endgame'[16])
